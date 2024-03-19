3bab5db2c0ca20a6a8b793b49b21a6d2e674c69a
LaurieLecomte <lalec31@manitou.ibis.ulaval.ca>
2024-03-19 17:23:36 -0400

    corrected SaNa to SaSa in srun command

--- 01_scripts/RepeatMasker_SaSa.sh
+++ 01_scripts/RepeatMasker_SaSa.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Run on Manitou
-# srun -p medium -c 6 --mem=50G --time=7-00:00:00 -J RepeatMasker_SaNa -o log/RepeatMasker_SaNa_%j.log /bin/sh 01_scripts/RepeatMasker_SaNa.sh &
+# srun -p medium -c 6 --mem=50G --time=7-00:00:00 -J RepeatMasker_SaSa -o log/RepeatMasker_SaSa_%j.log /bin/sh 01_scripts/RepeatMasker_SaSa.sh &
 
 
 # VARIABLES
