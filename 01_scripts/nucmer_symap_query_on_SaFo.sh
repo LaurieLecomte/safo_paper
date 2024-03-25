8f2e21dcc3a2d9bb15684b4906e64d1b2d606f35
LaurieLecomte <lalec31@manitou.ibis.ulaval.ca>
2024-03-25 12:51:37 -0400

    renamed nucmer script and corrected srun line

--- 01_scripts/nucmer_symap_query_on_SaFo.sh
+++ 01_scripts/nucmer_symap_query_on_SaFo.sh
@@ -5,19 +5,19 @@
 
 # I use the conda env mummer with version 4.0.0 installed
 
-# srun -p large -c 8 --mem=150G --time=21-00:00:00 -J nucmer_symap_SaMa_on_SaFo -o log/nucmer_symap_SaMa_on_SaFo_%j.log /bin/sh 01_scripts/nucmer_symap_SaMa_on_SaFo.sh SaMa &
-# srun -p large -c 8 --mem=150G --time=21-00:00:00 -J nucmer_symap_SaSa_on_SaFo -o log/nucmer_symap_SaSa_on_SaFo_%j.log /bin/sh 01_scripts/nucmer_symap_SaSa_on_SaFo.sh SaSa &
-# srun -p large -c 8 --mem=150G --time=21-00:00:00 -J nucmer_symap_SaNa_on_SaFo -o log/nucmer_symap_SaNa_on_SaFo_%j.log /bin/sh 01_scripts/nucmer_symap_SaNa_on_SaFo.sh SaNa &
+# srun -p medium -c 6 --mem=100G --time=7-00:00:00 -J nucmer_symap_SaMa_on_SaFo -o log/nucmer_symap_SaMa_on_SaFo_%j.log /bin/sh 01_scripts/nucmer_symap_query_on_SaFo.sh SaMa &
+# srun -p medium -c 6 --mem=100G --time=7-00:00:00 -J nucmer_symap_SaSa_on_SaFo -o log/nucmer_symap_SaSa_on_SaFo_%j.log /bin/sh 01_scripts/nucmer_symap_query_on_SaFo.sh SaSa &
+# srun -p medium -c 6 --mem=100G --time=7-00:00:00 -J nucmer_symap_SaNa_on_SaFo -o log/nucmer_symap_SaNa_on_SaFo_%j.log /bin/sh 01_scripts/nucmer_symap_query_on_SaFo.sh SaNa &
 
 
 # VARIABLES
-CPU=8
+CPU=6
 SPECIES=$1
 SYN_DIR="synteny"
 OUTPUT_DIR="$SYN_DIR/SaFo_$SPECIES"
 
 #QUERY="synteny/SaFo_"$SPECIES"/query_"$SPECIES".chrs_noMt_renamed.fasta.masked"
-QUERY=$(find synten$SYN_DIR/SaFo_"$SPECIES" -type f -iname "query_$SPECIES.chrs_*renamed.fasta.masked")
+QUERY=$(find $SYN_DIR/SaFo_"$SPECIES" -type f -iname "query_$SPECIES.chrs_*renamed.fasta.masked")
 REF="$SYN_DIR/ref_SaFo.chrs_noMt_renamed.fasta"
 
 
