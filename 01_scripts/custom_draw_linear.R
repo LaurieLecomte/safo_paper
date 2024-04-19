fbebbe00c4350701b99b2db275d36d43129f2484
LaurieLecomte <lalec31@manitou.ibis.ulaval.ca>
2024-04-18 20:51:53 -0400

    added synteny plotting scripts

---
+++ 01_scripts/custom_draw_linear.R
@@ -0,0 +1,302 @@
+draw_linear_laurie <-
+  function (output,
+            sizefile,
+            ...,
+            directory = NULL,
+            fileformat = "png",
+            colours = colours.default,
+            w = 13,
+            h = 5,
+            opacity = 0.5) {
+    if (is.null(directory)) {
+      directory <- tempdir()
+    }
+    synteny.data.reframing <-
+      function(data, tar.y, ref.y, compiled.size) {
+        synteny <- data.frame()
+        for (i in c(1:nrow(data))) {
+          reference <- data[i, "ref.species"]
+          target <- data[i, "tar.species"]
+          tar_chr <- data[i, "tarchr"]
+          ref_chr <- data[i, "refchr"]
+          dir <- data[i, "dir"]
+          tar_sizes <- compiled.size[compiled.size$species ==
+                                       target,]
+          names(tar_sizes) <- c("tarchr", "size", "species",
+                                "xstart", "xend")
+          ref_sizes <- compiled.size[compiled.size$species ==
+                                       reference,]
+          names(ref_sizes) <- c("refchr", "size", "species",
+                                "xstart", "xend")
+          tar_add <- tar_sizes[as.character(tar_sizes$tarchr) ==
+                                 as.character(tar_chr),]$xstart
+          ref_add <- ref_sizes[as.character(ref_sizes$refchr) ==
+                                 as.character(ref_chr),]$xstart
+          tar_y <- tar.y
+          ref_y <- ref.y
+          tar_xstart <- data[i, "tarstart"] + tar_add
+          tar_xend <- data[i, "tarend"] + tar_add
+          ref_xstart <- data[i, "refstart"] + ref_add
+          ref_xend <- data[i, "refend"] + ref_add
+          inverted <- grepl("-", dir, fixed = TRUE)
+          if (inverted == TRUE) {
+            df <- data.frame(
+              x = c(tar_xstart, tar_xend,
+                    ref_xstart, ref_xend),
+              y = c(tar_y, tar_y,
+                    ref_y, ref_y),
+              fill = ref_chr,
+              group = paste0("s",
+                             i),
+              ref = reference,
+              tar = target
+            )
+          }
+          else {
+            df <- data.frame(
+              x = c(tar_xstart, ref_xstart,
+                    ref_xend, tar_xend),
+              y = c(tar_y, ref_y, ref_y,
+                    tar_y),
+              fill = ref_chr,
+              group = paste0("s",
+                             i),
+              ref = reference,
+              tar = target
+            )
+          }
+          synteny <- rbind(synteny, df)
+        }
+        return(synteny)
+      }
+    colours.default <-
+      c(
+        `1` = "#BFD73B",
+        `2` = "#39ACE2",
+        `3` = "#F16E8A",
+        `4` = "#2DB995",
+        `5` = "#855823",
+        `6` = "#A085BD",
+        `7` = "#2EB560",
+        `8` = "#D79128",
+        `9` = "#FDBB63",
+        `10` = "#AFDFE5",
+        `11` = "#BF1E2D",
+        `12` = "purple4",
+        `13` = "#B59F31",
+        `14` = "#F68B1F",
+        `15` = "#EF374B",
+        `16` = "#D376FF",
+        `17` = "#009445",
+        `18` = "#CE4699",
+        `19` = "#7C9ACD",
+        `20` = "#84C441",
+        `21` = "#404F23",
+        `22` = "#607F4B",
+        `23` = "#EBB4A9",
+        `24` = "#F6EB83",
+        `25` = "#915F6D",
+        `26` = "#602F92",
+        `27` = "#81CEC6",
+        `28` = "#F8DA04",
+        `29` = "peachpuff2",
+        `30` = "gray85",
+        `33` = "peachpuff3",
+        W = "#9590FF",
+        Z = "#666666",
+        Y = "#9590FF",
+        X = "#666666",
+        LGE22 = "grey",
+        LGE64 = "gray64",
+        `1A` = "pink",
+        `1B` = "dark blue",
+        `4A` = "light green",
+        Gap = "white",
+        LG2 = "black",
+        LG5 = "#CC99CC"
+      )
+    xstart <-
+      xend <-
+      refchr <-
+      tarchr <- x <- y <- group <- fill <- chromosome <- species <- NULL
+    sizes <- utils::read.delim(sizefile, header = FALSE)
+    names(sizes) <- c("chromosome", "size", "species")
+    sizes$size <- as.numeric(gsub(",", "", sizes$size))
+    count <- 0
+    compiled.size <- data.frame()
+    for (i in unique(sizes$species)) {
+      size.intermediate <- sizes[sizes$species == i,]
+      for (x in c(1:nrow(size.intermediate))) {
+        if (x == 1) {
+          total_start <- 1
+          total_end <- size.intermediate[x, "size"]
+        }
+        else {
+          total_start <- total_end + 6e+06
+          total_end <- total_start + size.intermediate[x,
+                                                       "size"]
+        }
+        size.intermediate[x, "xstart"] <- total_start
+        size.intermediate[x, "xend"] <- total_end
+      }
+      compiled.size <- rbind(compiled.size, size.intermediate)
+    }
+    for (z in unique(compiled.size$species)) {
+      compiled.size$y[compiled.size$species == z] <- count
+      count <- count + 2
+    }
+    list.of.files <- list()
+    for (i in list(...)) {
+      list.of.files[[i]] <- i
+    }
+    listsynt <- list()
+    for (i in 1:length(list.of.files)) {
+      num <- i
+      file <- list.of.files[[num]]
+      dataTMP <- utils::read.delim(file, header = FALSE)
+      data2 <- dataTMP[, c(4, 5, 6, 1, 2, 3, 7, 8, 9)]
+      colnames(data2) <- c(
+        "tarchr",
+        "tarstart",
+        "tarend",
+        "refchr",
+        "refstart",
+        "refend",
+        "dir",
+        "ref.species",
+        "tar.species"
+      )
+      data2$tarstart <- as.numeric(gsub(",", "", data2$tarstart))
+      data2$tarend <- as.numeric(gsub(",", "", data2$tarend))
+      data2$refstart <- as.numeric(gsub(",", "", data2$refstart))
+      data2$refend <- as.numeric(gsub(",", "", data2$refend))
+      reference <- data2[1, "ref.species"]
+      target <- data2[1, "tar.species"]
+      ref_y <- compiled.size[compiled.size$species == reference,
+                             "y"]
+      tar_y <- compiled.size[compiled.size$species == target,
+                             "y"]
+      if (tar_y[1] > ref_y[1]) {
+        ref_y <- ref_y[1] + 0.1
+        tar_y <- tar_y[1]
+      }
+      else {
+        ref_y <- ref_y[1]
+        tar_y <- tar_y[1] + 0.1
+      }
+      x <- synteny.data.reframing(data2, tar_y, ref_y, compiled.size)
+      x$fill <- as.factor(x$fill)
+      listsynt[[i]] <- x
+    }
+    compiled.size$chromosome <- as.factor(compiled.size$chromosome)
+    
+    p <- ggplot2::ggplot()
+    for (i in 1:length(listsynt)) {
+      data <- listsynt[[i]]
+      reference <- data[1, "ref"]
+      target <- data[1, "tar"]
+      ref_sizes <- compiled.size[compiled.size$species == reference,]
+      tar_sizes <- compiled.size[compiled.size$species == target,]
+      p <-
+        p +
+        ggplot2::geom_rect(
+          data = ref_sizes,
+          mapping = ggplot2::aes(
+            xmin = xstart,
+            xmax = xend,
+            ymin = y,
+            ymax = y + 0.1,
+            fill = chromosome
+          ),
+          color = "black",
+          alpha = 0.85,
+          linewidth = 0.1
+        ) +
+        
+        ggplot2::geom_polygon(
+          data = data,
+          alpha = opacity,
+          ggplot2::aes(
+            x = x,
+            y = y,
+            group = group,
+            fill = fill
+          )
+        ) +
+        # Ref chr tags
+        ggplot2::geom_text(
+          data = ref_sizes,
+          ggplot2::aes(
+            x = (xstart + xend) / 2,
+            y = y+0.2,
+            label = chromosome
+          ),
+          size = 3,
+          angle = 0,
+        ) +
+        
+        # Ref pecies tag
+        ggplot2::geom_text(
+          data = ref_sizes,
+          mapping = ggplot2::aes(x = 2, y = y, label = species),
+          size = 3,
+          hjust = 1,
+          vjust = 1
+        ) +
+        # Query chr rectangles
+        ggplot2::geom_rect(
+          data = tar_sizes,
+          mapping = ggplot2::aes(
+            xmin = xstart,
+            xmax = xend,
+            ymin = y,
+            ymax = y + 0.1
+          ),
+          fill = "grey90",
+          color = "black",
+          alpha = 0.85,
+          linewidth = 0.1
+        ) +
+        # Query chr rectangles
+        ggplot2::geom_text(
+          data = tar_sizes,
+          ggplot2::aes(
+            x = (xstart + xend) / 2,
+            y = y + 0.05,
+            label = chromosome
+          ),
+          size = 2,
+          angle = 45
+        ) +
+        # Query species tag
+        ggplot2::geom_text(
+          data = tar_sizes,
+          mapping = ggplot2::aes(x = 2, y = y, label = species),
+          size = 3,
+          hjust = 1)
+          
+        
+    }
+    p <- p +
+      ggplot2::scale_fill_manual(values = colours) +
+      ggplot2::theme(
+        panel.background = ggplot2::element_blank(),
+        strip.background = ggplot2::element_blank(),
+        axis.title.y = ggplot2::element_blank(),
+        axis.title.x = ggplot2::element_blank(),
+        axis.text.x = ggplot2::element_blank(),
+        axis.text.y = ggplot2::element_blank(),
+        axis.ticks.x = ggplot2::element_blank(),
+        axis.ticks.y = ggplot2::element_blank(),
+        legend.position = "none"
+      )
+    message(paste0("Saving linear image to ", directory))
+    print(p)
+    ggplot2::ggsave(
+      paste0(directory, "/", output, ".", fileformat),
+      p,
+      device = fileformat,
+      width = w,
+      height = h
+    )
+  }
--- 01_scripts/test_syntenyPlotterR.R
+++ 01_scripts/test_syntenyPlotterR.R
@@ -19,17 +19,30 @@ rchr_lengths$ID <- 'SaFo'
 
 # 2. Import files for query SaNa ------------------------------------------
 # Alignment
-blocks <- read.delim("~/SaFo_paper/querySaNa_to_refSaFo/results/blocks",
+q1_blocks <- read.delim("~/SaFo_paper/querySaNa_to_refSaFo/results/blocks",
                      col.names = c('qChr', 'rChr', 'block', 'qStart', 'qEnd', 'rStart', 'rEnd', 'hits', 'qGenes', 'rGenes', 'qPctGenes', 'rPctGenes'))
 
 
-blocks_fmt <- blocks[, c('rChr', 'rStart', 'rEnd', 'qChr', 'qStart', 'qEnd')]
-blocks_fmt$orient <- '+'
-blocks_fmt$rID <- 'SaFo'
-blocks_fmt$qID <- 'SaNa'
+for (i in 1:nrow(q1_blocks)){
+  q1_blocks$ref_block_size[i] <- (q1_blocks$rEnd[i] - q1_blocks$rStart[i])
+  q1_blocks$ref_chr_size[i] <- rchr_lengths$size[rchr_lengths$chrNum == q1_blocks$rChr[i]]
+  q1_blocks$ref_chr_prop[i] <- q1_blocks$ref_block_size[i]/q1_blocks$ref_chr_size[i]
+  
+}
+
+q1_blocks <- q1_blocks[order(q1_blocks$rChr),]
+
+# Remove blocks smaller than 100 kb
+q1_blocks <- subset(q1_blocks, ref_block_size > 5000000)
+
+q1_blocks_fmt <- q1_blocks[, c('rChr', 'rStart', 'rEnd', 'qChr', 'qStart', 'qEnd')]
+q1_blocks_fmt$orient <- '+'
+q1_blocks_fmt$rID <- 'SaFo'
+q1_blocks_fmt$qID <- 'SaNa'
 
 
-write.table(blocks_fmt, file = "~/SaFo_paper/querySaNa_to_refSaFo/results/blocks_fmt.txt",
+
+write.table(q1_blocks_fmt, file = "~/SaFo_paper/querySaNa_to_refSaFo/results/blocks_fmt.txt",
             row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
 
 #less "synteny/SaFo_SaNa/SaNa.chrs_noMt.fasta.fai" | cut -f1,2 | sed -E 's/^(CM.+)\:0\-[0-9]+/\1/'  > "synteny/SaFo_SaNa/SaNa.chrs_noMt.chrsizes.txt"
@@ -47,9 +60,98 @@ q1chr_lengths <- merge(q1chrsizes, q1chrs_for_blocks, by.x = 'chr', by.y = 'chrC
 q1chr_lengths <- q1chr_lengths[, c('chrNum', 'size')]
 q1chr_lengths$ID <- 'SaNa'
 
+
+q1chr_lengths <- q1chr_lengths[order(match(q1chr_lengths$chrNum, unique(q1_blocks_fmt$qChr))),]
+
 #all_chrlengths <- rbind(q1chr_lengths, rchr_lengths)
 #write.table(all_chrlengths, file = "~/SaFo_paper/querySaNa_to_refSaFo/SaNa_SaFo.chrlengths.txt",
 #            row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
+
+
+# 3. Import files for query SaSa ------------------------------------------
+# Alignment
+q2_blocks <- read.delim("~/SaFo_paper/querySaSa_to_refSaFo/results/blocks",
+                     col.names = c('qChr', 'rChr', 'block', 'qStart', 'qEnd', 'rStart', 'rEnd', 'hits', 'qGenes', 'rGenes', 'qPctGenes', 'rPctGenes'))
+
+for (i in 1:nrow(q2_blocks)){
+  q2_blocks$ref_block_size[i] <- (q2_blocks$rEnd[i] - q2_blocks$rStart[i])
+  q2_blocks$ref_chr_size[i] <- rchr_lengths$size[rchr_lengths$chrNum == q2_blocks$rChr[i]]
+  q2_blocks$ref_chr_prop[i] <- q2_blocks$ref_block_size[i]/q2_blocks$ref_chr_size[i]
+  
+}
+
+
+
+
+q2_blocks <- subset(q2_blocks, ref_block_size > 5000000)
+
+# Find the biggest block for each chr
+q2_big_blocks <- group_by(q2_blocks, rChr) %>% top_n(1, ref_chr_prop)
+
+q2_blocks <- rbind(q2_big_blocks, setdiff(q2_blocks, q2_big_blocks))
+
+q2_blocks <- q2_blocks[order(q2_big_blocks$rChr),]
+
+q2_blocks_fmt <- q2_blocks[, c('rChr', 'rStart', 'rEnd', 'qChr', 'qStart', 'qEnd')]
+q2_blocks_fmt$orient <- '+'
+q2_blocks_fmt$rID <- 'SaFo'
+q2_blocks_fmt$qID <- 'SaSa'
+
+
+write.table(q2_blocks_fmt, file = "~/SaFo_paper/querySaSa_to_refSaFo/results/blocks_fmt.txt",
+            row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
+
+#less "synteny/SaFo_SaSa/SaSa.chrs.fasta.fai" | cut -f1,2 | sed -E 's/^(HG.+)\:0\-[0-9]+/\1/'  > "synteny/SaFo_SaSa/SaSa.chrs.chrsizes.txt"
+q2chrsizes <- read.delim("~/SaFo_paper/querySaSa_to_refSaFo/SaSa.chrs.chrsizes.txt", header=FALSE,
+                         col.names = c('chr', 'size'))
+
+#less "synteny/SaFo_SaSa/SaSa.chrs.corrsp.txt" | sed -E 's/^(HG.+)\:0\-[0-9]+/\1/' > "synteny/SaFo_SaSa/SaSa.chrs_for_blocks.txt"
+q2chrs_for_blocks <- read.delim("~/SaFo_paper/querySaSa_to_refSaFo/SaSa.chrs_for_blocks.txt", header=FALSE,
+                                col.names = c('chrHG', 'chrSymap'))
+
+q2chrs_for_blocks$chrNum <- 1:nrow(q2chrs_for_blocks)
+
+
+q2chr_lengths <- merge(q2chrsizes, q2chrs_for_blocks, by.x = 'chr', by.y = 'chrHG', sort = FALSE)
+q2chr_lengths <- q2chr_lengths[, c('chrNum', 'size')]
+q2chr_lengths$ID <- 'SaSa'
+
+#q2chr_lengths <- q2chr_lengths[order(match(q2chr_lengths$chrNum, unique(q2_big_blocks$qChr))),]
+q2chr_lengths <- q2chr_lengths[order(match(q2chr_lengths$chr, unique(q2_blocks_fmt$qChr))),]
+
+# 4. Test syntenyPlotteR --------------------------------------------------
+all_chrlengths <- rbind(q1chr_lengths, rchr_lengths, q2chr_lengths)
+
+write.table(all_chrlengths, file = "~/SaFo_paper/all_chrlengths.txt",
+            row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
+
+
+
+library(syntenyPlotteR)
+draw.linear(directory = "~/SaFo_paper",
+            paste0('test', format(Sys.time(), "%Y%m%d.%H%M%S")),
+            sizefile = "~/SaFo_paper/all_chrlengths.txt", 
+            "~/SaFo_paper/querySaNa_to_refSaFo/results/blocks_fmt.txt",
+            "~/SaFo_paper/querySaSa_to_refSaFo/results/blocks_fmt.txt",
+            colours = rep(c('black', 'blue'), (nrow(all_chrlengths)+1)/2))
+
+draw_linear_laurie(
+  directory = "~/SaFo_paper",
+  paste0('test', format(Sys.time(), "%Y%m%d.%H%M%S")),
+            sizefile = "~/SaFo_paper/all_chrlengths.txt", 
+            "~/SaFo_paper/querySaNa_to_refSaFo/results/blocks_fmt.txt",
+            "~/SaFo_paper/querySaSa_to_refSaFo/results/blocks_fmt.txt",
+            colours = rep(c('grey20', 'blue'), (nrow(all_chrlengths)+1)/2))
+
+
+
+
+
+
+
+
+
+
 
 
 # 3. Import files for query SaMa ------------------------------------------
@@ -88,7 +190,7 @@ q2chr_lengths$ID <- 'SaMa'
 
 
 
-# 4. Test syntenyPlotteR --------------------------------------------------
+
 all_chrlengths <- rbind(q1chr_lengths, rchr_lengths, q2chr_lengths)
 
 write.table(all_chrlengths, file = "~/SaFo_paper/all_chrlengths.txt",
@@ -103,3 +205,206 @@ draw.linear(directory = "~/SaFo_paper",
             "~/SaFo_paper/querySaNa_to_refSaFo/results/blocks_fmt.txt",
             "~/SaFo_paper/querySaMa_to_refSaFo/results/blocks_fmt.txt",
             colours = rep(c('black', 'blue'), (nrow(all_chrlengths)+1)/2))
+
+draw_linear_laurie(directory = "~/SaFo_paper",
+            'test',
+            sizefile = "~/SaFo_paper/all_chrlengths.txt", 
+            "~/SaFo_paper/querySaNa_to_refSaFo/results/blocks_fmt.txt",
+            "~/SaFo_paper/querySaMa_to_refSaFo/results/blocks_fmt.txt",
+            colours = rep(c('black', 'blue'), (nrow(all_chrlengths)+1)/2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# All sizes together ------------------------------------------------------
+rchrsizes <- read.delim("~/SaFo_paper/SaFo.chrs_noMt.chrsizes.txt", header=FALSE,
+                        col.names = c('chr', 'size'))
+
+rchrs_for_blocks <- read.delim("~/SaFo_paper/SaFo.chrs_for_blocks.txt", header=FALSE,
+                               col.names = c('chrCM', 'chrSymap'))
+rchrs_for_blocks$chrNum <- 1:nrow(rchrs_for_blocks)
+
+rchr_lengths <- merge(rchrsizes, rchrs_for_blocks, by.x = 'chr', by.y = 'chrCM', sort = FALSE)
+rchr_lengths$ID <- 'SaFo'
+
+#less "synteny/SaFo_SaNa/SaNa.chrs_noMt.fasta.fai" | cut -f1,2 | sed -E 's/^(CM.+)\:0\-[0-9]+/\1/'  > "synteny/SaFo_SaNa/SaNa.chrs_noMt.chrsizes.txt"
+q1chrsizes <- read.delim("~/SaFo_paper/querySaNa_to_refSaFo/SaNa.chrs_noMt.chrsizes.txt", header=FALSE,
+                         col.names = c('chr', 'size'))
+
+#less "synteny/SaFo_SaNa/SaNa.chrs_noMt.corrsp.txt" | sed -E 's/^(CM.+)\:0\-[0-9]+/\1/' > "synteny/SaFo_SaNa/SaNa.chrs_for_blocks.txt"
+q1chrs_for_blocks <- read.delim("~/SaFo_paper/querySaNa_to_refSaFo/SaNa.chrs_for_blocks.txt", header=FALSE,
+                                col.names = c('chr', 'chrSymap'))
+
+q1chrs_for_blocks$chrNum <- 1:nrow(q1chrs_for_blocks)
+
+
+q1chr_lengths <- merge(q1chrsizes, q1chrs_for_blocks, by = 'chr', sort = FALSE)
+#q1chr_lengths <- q1chr_lengths[, c('chrNum', 'size')]
+q1chr_lengths$ID <- 'SaNa'
+
+
+#q1chr_lengths <- q1chr_lengths[order(match(q1chr_lengths$chrNum, unique(q1_blocks_fmt$qChr))),]
+
+
+
+#less "synteny/SaFo_SaSa/SaSa.chrs.fasta.fai" | cut -f1,2 | sed -E 's/^(HG.+)\:0\-[0-9]+/\1/'  > "synteny/SaFo_SaSa/SaSa.chrs.chrsizes.txt"
+q2chrsizes <- read.delim("~/SaFo_paper/querySaSa_to_refSaFo/SaSa.chrs.chrsizes.txt", header=FALSE,
+                         col.names = c('chr', 'size'))
+
+#less "synteny/SaFo_SaSa/SaSa.chrs.corrsp.txt" | sed -E 's/^(HG.+)\:0\-[0-9]+/\1/' > "synteny/SaFo_SaSa/SaSa.chrs_for_blocks.txt"
+q2chrs_for_blocks <- read.delim("~/SaFo_paper/querySaSa_to_refSaFo/SaSa.chrs_for_blocks.txt", header=FALSE,
+                                col.names = c('chr', 'chrSymap'))
+
+q2chrs_for_blocks$chrNum <- 1:nrow(q2chrs_for_blocks)
+
+
+q2chr_lengths <- merge(q2chrsizes, q2chrs_for_blocks, by = 'chr', sort = FALSE)
+#q2chr_lengths <- q2chr_lengths[, c('chrNum', 'size')]
+q2chr_lengths$ID <- 'SaSa'
+
+#q2chr_lengths <- q2chr_lengths[order(match(q2chr_lengths$chrNum, unique(q2_big_blocks$qChr))),]
+
+
+#all_lengths_table <- rbind(q1chr_lengths, rchr_lengths, q2chr_lengths)
+
+
+
+# Query 1
+q1_blocks <- read.delim("~/SaFo_paper/querySaNa_to_refSaFo/results/blocks",
+                        col.names = c('qChr', 'rChr', 'block', 'qStart', 'qEnd', 'rStart', 'rEnd', 'hits', 'qGenes', 'rGenes', 'qPctGenes', 'rPctGenes'))
+
+q1_blocks$qID <- 'SaNa'
+
+
+for (i in 1:nrow(q1_blocks)){
+  q1_blocks$ref_block_size[i] <- (q1_blocks$rEnd[i] - q1_blocks$rStart[i])
+  q1_blocks$ref_chr_size[i] <- rchr_lengths$size[rchr_lengths$chrNum == q1_blocks$rChr[i]]
+  q1_blocks$ref_chr_prop[i] <- q1_blocks$ref_block_size[i]/q1_blocks$ref_chr_size[i]
+  q1_blocks$rChrCM[i] <- rchr_lengths$chr[rchr_lengths$chrNum == q1_blocks$rChr[i]]
+  q1_blocks$qChrCM[i] <- q1chr_lengths$chr[q1chr_lengths$chrNum == q1_blocks$qChr[i]]
+  
+}
+
+q1_blocks <- q1_blocks[order(q1_blocks$rChr),]
+
+# Remove blocks smaller than 100 kb
+q1_blocks <- subset(q1_blocks, ref_block_size > 20000000)
+
+#q1_blocks_fmt <- q1_blocks[, c('rChrCM', 'rStart', 'rEnd', 'qChrCM', 'qStart', 'qEnd')]
+q1_blocks_fmt <- q1_blocks[, c('rChr', 'rStart', 'rEnd', 'qChr', 'qStart', 'qEnd')]
+
+q1_blocks_fmt$orient <- '+'
+q1_blocks_fmt$rID <- 'SaFo'
+q1_blocks_fmt$qID <- 'SaNa'
+
+
+write.table(q1_blocks_fmt, file = "~/SaFo_paper/querySaNa_to_refSaFo/results/blocks_fmt1.txt",
+            row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
+
+
+#q1chr_lengths <- q1chr_lengths[order(match(q1chr_lengths$chr, unique(q1_blocks_fmt$qChrCM))),]
+q1chr_lengths <- q1chr_lengths[order(match(q1chr_lengths$chr, unique(q1_blocks_fmt$qChr))),]
+
+
+
+
+
+# Query 2
+q2_blocks <- read.delim("~/SaFo_paper/querySaSa_to_refSaFo/results/blocks",
+                        col.names = c('qChr', 'rChr', 'block', 'qStart', 'qEnd', 'rStart', 'rEnd', 'hits', 'qGenes', 'rGenes', 'qPctGenes', 'rPctGenes'))
+
+q2_blocks$qID <- 'SaSa'
+
+
+
+for (i in 1:nrow(q2_blocks)){
+  q2_blocks$ref_block_size[i] <- (q2_blocks$rEnd[i] - q2_blocks$rStart[i])
+  q2_blocks$ref_chr_size[i] <- rchr_lengths$size[rchr_lengths$chrNum == q2_blocks$rChr[i]]
+  q2_blocks$ref_chr_prop[i] <- q2_blocks$ref_block_size[i]/q2_blocks$ref_chr_size[i]
+  q2_blocks$rChrCM[i] <- rchr_lengths$chr[rchr_lengths$chrNum == q2_blocks$rChr[i]]
+  q2_blocks$qChrCM[i] <- q2chr_lengths$chr[q2chr_lengths$chrNum == q2_blocks$qChr[i]]
+  
+}
+
+q2_blocks <- q2_blocks[order(q2_blocks$rChr),]
+
+# Remove blocks smaller than 100 kb
+q2_blocks <- subset(q2_blocks, ref_block_size > 20000000)
+
+
+# Find the biggest block for each chr
+q2_big_blocks <- group_by(q2_blocks, rChr) %>% top_n(1, ref_chr_prop)
+
+#q2_big_blocks <- group_by(q2_blocks, rChr) %>% summarise(main_chr = max(ref_chr_pro), qChr = qChr)
+
+q2_blocks <- rbind(q2_big_blocks, setdiff(q2_blocks, q2_big_blocks))
+
+#q2_blocks <- q2_blocks[order(q2_big_blocks$rChr),]
+
+#q2_blocks_fmt <- q2_blocks[, c('rChrCM', 'rStart', 'rEnd', 'qChrCM', 'qStart', 'qEnd')]
+q2_blocks_fmt <- q2_blocks[, c('rChr', 'rStart', 'rEnd', 'qChr', 'qStart', 'qEnd')]
+
+q2_blocks_fmt$orient <- '+'
+q2_blocks_fmt$rID <- 'SaFo'
+q2_blocks_fmt$qID <- 'SaSa'
+
+
+
+write.table(q2_blocks_fmt, file = "~/SaFo_paper/querySaSa_to_refSaFo/results/blocks_fmt1.txt",
+            row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
+
+
+#q2chr_lengths <- q2chr_lengths[order(match(q2chr_lengths$chr, unique(q2_blocks_fmt$qChrCM))),]
+q2chr_lengths <- q2chr_lengths[order(match(q2chr_lengths$chr, unique(q2_blocks_fmt$qChr))),]
+
+
+
+
+
+all_chrlengths <- rbind(q1chr_lengths, 
+                        rchr_lengths, 
+                        q2chr_lengths)
+
+write.table(all_chrlengths[, c('chr', 'size', 'ID')], file = "~/SaFo_paper/all_chrlengths_CM.txt",
+            row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
+
+
+
+library(syntenyPlotteR)
+draw.linear(directory = "~/SaFo_paper",
+            paste0('test', format(Sys.time(), "%Y%m%d.%H%M%S")),
+            sizefile = "~/SaFo_paper/all_chrlengths_CM.txt", 
+            "~/SaFo_paper/querySaNa_to_refSaFo/results/blocks_fmt1.txt",
+            "~/SaFo_paper/querySaSa_to_refSaFo/results/blocks_fmt1.txt",
+            colours = rep(c('black', 'blue'), (nrow(all_chrlengths)+1)/2))
+
+
+
+draw_linear_laurie(directory = "~/SaFo_paper",
+            paste0('test', format(Sys.time(), "%Y%m%d.%H%M%S")),
+            sizefile = "~/SaFo_paper/all_chrlengths_CM.txt", 
+            "~/SaFo_paper/querySaNa_to_refSaFo/results/blocks_fmt1.txt",
+            "~/SaFo_paper/querySaSa_to_refSaFo/results/blocks_fmt1.txt",
+            colours = rep(c('black', 'blue'), (nrow(all_chrlengths)+1)/2))
+
+
+
+
+
+
+# With manual order -------------------------------------------------------
+q1_chr_order <- c(4,3,2,1,8,5,7,16,21,26,6,9,12,10,15,28,11,20,19,29,31,27,13,23,33,25,24,18,17,35,14,32,34,41,30,36,22,37,38,39,40,42)
+
+
+
+
