7a9a8e967e84648db017cffe64e82cb07e7eabf1
LaurieLecomte <lalec31@manitou.ibis.ulaval.ca>
2025-03-10 17:25:53 -0400

    added plotting steps

--- 01_scripts/explore_mapped_transcripts.R
+++ 01_scripts/explore_mapped_transcripts.R
@@ -130,17 +130,6 @@ mapped_uniques_wide_80 <- tidyr::pivot_wider(mapped_uniques_80[!duplicated(mappe
                                           names_from = species,
                                           values_from = cp_number)
 
-# Get distance between species
-out_dist <- expand.grid('species1' = df_names, 'species2' = df_names)
-for (i in 1:nrow(out_dist)){
-  out_dist$dist[i] <- sum(abs(mapped_uniques_wide_80[[out_dist$species1[i]]] - mapped_uniques_wide_80[[out_dist$species2[i]]]))/nrow(mapped_uniques_wide_80)
-}
-
-tidyr::pivot_wider(out_dist, 
-                   names_from = c('species1'),
-                   values_from = 'dist')
-
-
 
 # Plot distrib
 ggplot(data = mapped_uniques_80) + 
@@ -149,28 +138,33 @@ ggplot(data = mapped_uniques_80) +
   lims(x = c(60, 102))
 
 # Plot copy number within asms
-ggplot(data = mapped_uniques_80) + 
-  geom_bar(aes(x = species, fill = factor(cp_number))) +
-  scale_fill_viridis_d(option = 'D')
+mapped_barplot <- 
+ggplot(data = mapped_uniques_80, aes(x = factor(species, level = c('safo', 'sana', 'sama', 'omyk', 'satr', 'sasaICSASG', 'cocl')))) + 
+  geom_bar(aes(fill = factor(cp_number))) +
+  scale_fill_viridis_d(option = 'H') + 
+  labs(fill='Transcript copy number', x = 'Species', y = 'Transcript count') +
+  scale_x_discrete(labels = c(
+    "safo" = "Salvelinus fontinalis", 
+    "sana" = "Salvelinus namaycush", 
+    "sama" = "Salvelinus sp.",
+    "omyk" = "Oncorhynchus mykiss",
+    "satr" = "Salmo trutta",
+    "sasaICSASG" = "Salmo salar",
+    "cocl" = "Coregonus clupeaformis")) +
+  scale_y_continuous(labels = scales::number_format(big.mark = ",")) +
+  theme(
+    axis.text.x = element_text(face = 'italic', angle = 45, size = 9, hjust = 1),
+    axis.text.y = element_text(size = 9, hjust = 1),
+    legend.title = element_text(size = 9),
+    panel.border = element_rect(color = 'black', fill = NA, linewidth = 0.2),
+    panel.background = element_blank()
+  )
 
+ggsave(filename = 'species_comparison/transcriptome/mapped_transcripts_over80idy_barplot.pdf',
+       mapped_barplot, width = 10, height = 8)
 
-
-mapped_dist <- dist(mapped_uniques_wide_80[, 2:8])
-
-
-missing_transcripts_safo <- subset(mapped_uniques_wide_80, safo == 0)
-duplicated_transcripts_safo <- subset(mapped_uniques_wide_80, safo > 1)
-
-
-
-for (i in 1:nrow(mapped_uniques_wide_80)){
-  mapped_uniques_wide_80$single_copy[i] <- sum(mapped_uniques_wide_80[i, 2:8] == 1)
-  mapped_uniques_wide_80$duplicated[i] <- sum(mapped_uniques_wide_80[i, 2:8] > 1)
-  mapped_uniques_wide_80$missing[i] <- sum(mapped_uniques_wide_80[i, 2:8] == 0)
-}
-
-missing_safo_transcripts <- subset(mapped_uniques_wide_80, safo == 0)
-duplicated_safo <- subset(mapped_uniques_wide, safo > 1)
+#missing_safo_transcripts <- subset(mapped_uniques_wide_80, safo == 0)
+#duplicated_safo <- subset(mapped_uniques_wide, safo > 1)
 
 
 
@@ -179,160 +173,3 @@ write.table(mapped_uniques_wide_80, file = 'species_comparison/transcriptome/map
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-all_mapped_wide <- tidyr::pivot_wider(all_mapped[!duplicated(all_mapped[, c('name', 'species')]), c('name', 'species',  'cp_number')], 
-                                    names_from = species,
-                                    values_from = cp_number)
-
-
-
-out_df <- data_frame(name = unique(Ssal_v3.1_rna$name))
-
-for (sp in c('safo', 'sana', 'sama', 'omyk', 'satr', 'sasaICSASG', 'cocl')){
-  for (i in unique(Ssal_v3.1_rna$name)){
-    df <- subset(all_mapped, name == i & species == sp)
-    if (all(df$cp_number == 1)) {
-      print(paste0('single copy (', df$identity, ')'))
-    } else if (nrow(df) > 1) {
-      print(paste0('duplicated (', cat(df$identity, sep = ','), ')'))
-    } else if (df$cp_number == 0){
-      print('missing')
-    }
-    }
-}
-
-out_df <- data_frame(name = unique(Ssal_v3.1_rna$name))
-
-
-
-
-
-
-
-
-
-
-
-
-for (sp in c('safo', 'sana', 'sama', 'omyk', 'satr', 'sasaICSASG', 'cocl')){
-  
-  #assign(x = paste0(sp, '_status'), value = vector(mode = 'character', length = nrow(Ssal_v3.1_rna)))
-  status_vec <- vector(mode = 'character', length = nrow(Ssal_v3.1_rna))
-  idy_vec <- vector(mode = 'character', length = nrow(Ssal_v3.1_rna))
-  sp_df <- subset(all_mapped, species == sp)
-  
-  for (i in unique(Ssal_v3.1_rna$name)){
-    
-    df <- subset(sp_df, name == i)
-    #print(cat(df$identity, sep = ','))
-    status_vec[i] <- 
-      (ifelse(test = all(df$cp_number == 1), yes = 'single copy', 
-                            no = ifelse(test = all(df$cp_number > 1), yes = 'duplicated', no = 'missing')))
-    idy_vec <- 
-      (paste(df$identity))
-  }
-  assign(x = paste0('status_', sp), value = status_vec)
-  assign(x = paste0('idy_', sp), value = idy_vec)
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-all_mapped_wider <- tidyr::pivot_wider(all_mapped[, c('name', 'species',  'cp_number', 'identity')], 
-                                      names_from = species,
-                                      values_from = cp_number)
-
-
-tidyr::pivot_wider(all_mapped[, c('name', 'species',  'cp_number', 'identity')], 
-                   names_from = species,
-                   values_from = c(cp_number, identity)
-                   )
-
-
-mapped_uniques_wide$single_copy <- NA
-
-
-safo_s <- subset(all_mapped, species == 'safo')
-
-for (i in unique(safo_s$name)){
-  if (safo_s$cp_number[safo_s$name == i] == 1){
-    
-    print(paste0('Single copy (', safo_s$identity[safo_s$name == i], ')'))
-  } 
-}
-
-for (i in 1:nrow(all_mapped)){
-  if (all_mapped$cp_number[i] == 1){
-    print(paste0('single copy (', all_mapped$identity, ')'))
-  }
-  
-}
-
-
-
-# Poubelle ----------------------------------------------------------------
-
-
-# Transcripts
-Ssal_v3.1_rna <- read.delim("species_comparison/transcriptome/GCF_905237065.1_Ssal_v3.1_rna.txt", header=FALSE,
-                                            col.names = c('name', 'full_name'))
-
-length(setdiff(Ssal_v3.1_rna$name, sasaICSASG$name))
-# 185 unmapped transcripts 
-# 112712 mapped transcript names
-
-length(intersect(Ssal_v3.1_rna$name, dfs[[5]]$name))
-length(setdiff(Ssal_v3.1_rna$name, dfs[[5]]$name))
-
-# add copy number info
-satr_counts <- dfs[[5]] %>% count(name)
-satr_merged <- merge(x = dfs[[5]], y = satr_counts, by = 'name')
-
-# add present-absent info  
-satr_merged <- merge(x = satr_merged, y = Ssal_v3.1_rna, by = 'name', all.y = TRUE) # faire avant ajout variable species
-satr_merged$status <- ifelse(test = is.na(satr_merged$identity), yes = 'absent', no = 'present')
-## add cp number = 0 if absent
-satr_merged$cp_number <- ifelse(test = satr_merged$status == 'absent',
-                                yes = 0, no = satr_merged$n)
-
-
-
-
-dfs[[5]][ dfs[[5]]$name %in% dfs[[5]]$name[duplicated(dfs[[5]]$name)], ] # duplicated
-dfs[[5]][ ! dfs[[5]]$name %in% dfs[[5]]$name[duplicated(dfs[[5]]$name)], ] # single copy
-
-dfs[[5]][ dfs[[5]]$name %in% setdiff(Ssal_v3.1_rna$ID, dfs[[5]]$name), ]
-
-
-
-
-library(ggplot2)
-
-
-for (i in dfs){
-  print(median(dfs[['identity']]))
-}
